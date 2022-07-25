149025ebe456e2616ffe9e88969203b396cd18c7
titulebolide <seneztit@gmail.com>
2022-07-25 19:23:12 +0200

    fix main

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import time
 from urllib.parse import unquote
 import markdownify
 import config
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+logging.info("Starting")
 
 refresh_rate = datetime.timedelta(hours=1)
 last_successful_fetch_date = datetime.datetime.now()
@@ -26,7 +31,7 @@ def get_user_name(id):
     try:
         name = res['name']
     except IndexError:
-        print("error for name")
+        logging.warning("error for name")
         name = "?"
     return name
 
@@ -37,7 +42,7 @@ def get_media_url(id):
     try:
         url = resp['media_details']['sizes']['medium']['source_url']
     except IndexError:
-        print("error for media url")
+        logging.warning("error for media url")
         url = ""
     return url
 
@@ -73,18 +78,22 @@ def send_msg(message):
     )
     print(res.text)
 
+logging.debug("Fetching already existing articles")
 already_seen_posts.extend(
-    [post['id'] for post in get_recent_posts()]
+    [post['id'] for post in get_recent_posts(last_successful_fetch_date - datetime.timedelta(seconds=110))]
 )
+
+logging.info("Started")
 
 while True:
     time.sleep(refresh_rate.seconds)
+    logging.info(f"Fetching {config.WEBSITE}")
     fetch_date = datetime.datetime.now()
     posts = []
     try:
         posts = get_recent_posts(last_successful_fetch_date - datetime.timedelta(seconds=100))
     except Exception as e:
-        print(e)
+        logging.warning(e)
         continue
     last_successful_fetch_date = fetch_date
     for post in posts:
@@ -94,3 +103,4 @@ while True:
         already_seen_posts.append(post['id'])
         send_msg(msg)
     
+logging.info("Exiting.")
